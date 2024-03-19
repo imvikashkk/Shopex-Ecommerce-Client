4cb303a9fa52296286dcc73508e41fd71c7b54c5
Vikash Kumar <imvikashkk@gmail.com>
2024-03-19 18:10:33 +0530

    Changes in OrderSuccessPage & OrderFailedPage

--- src/pages/OrderFailedPage.jsx
+++ src/pages/OrderFailedPage.jsx
@@ -1,61 +1,69 @@
 import { useEffect } from "react";
-import { useNavigate, Navigate, useParams, useLocation } from "react-router-dom";
+import {
+  useNavigate,
+  Navigate,
+  useParams,
+  useLocation,
+} from "react-router-dom";
 import { resetCartAsync } from "../features/cart/cartSlice";
 import { useDispatch } from "react-redux";
 import { resetOrder } from "../features/order/orderSlice";
 
 function OrderFailedPage() {
-   const params = useParams() 
-   const dispatch = useDispatch();
-   const navigate = useNavigate();
-   const data = useLocation().state
+  const params = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const data = useLocation().state;
 
-   useEffect(()=>{
-    if(!data){
-      navigate("/" , {replace:true})
+  useEffect(() => {
+    if (!data) {
+      navigate("/", { replace: true });
     }
-   },[])
+  }, []);
 
-   useEffect(()=>{
-    dispatch(resetCartAsync())
-    dispatch(resetOrder())
-   },[dispatch])
+  useEffect(() => {
+    dispatch(resetCartAsync());
+    dispatch(resetOrder());
+  }, [dispatch]);
 
-   
   return (
     <>
-    {!params.id &&  <Navigate to='/' replace={true}></Navigate>}
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-2xl font-bold text-red-600">Order Failed !!</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-red-700 sm:text-5xl">
-          Order Number #{params?.id}
-        </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-           Code:{data?.code}
-        </p>
-        <p className="text-base leading-7 text-gray-600">
-          Description: {data?.description}
-        </p>
-        <p className="text-base leading-7 text-gray-600">
-          Source: {data?.source}
-        </p>
-        <p className="text-base leading-7 text-gray-600">
-           Step: {data?.step}
-        </p>
-        <p className="text-base leading-7 text-gray-600">
-          Reason: {data?.reason}
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <button
-            onClick={()=>navigate("/", {replace:true})}
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
-          >
-            Go back home
-          </button>
+      {!params.id && <Navigate to="/" replace={true}></Navigate>}
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-2xl font-bold text-red-600">Order Failed !!</p>
+          <div className="mt-4 flex flex-col justify-center items-center lg:flex-row lg:gap-2">
+            <h1 className="text-xl sm:3xl font-bold tracking-tight text-red-700">
+              Order Number
+            </h1>
+            <h1 className="text-xl sm:3xl font-bold tracking-tight text-red-700 ">
+              #{params?.id}
+            </h1>
+          </div>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            Code:{data?.code}
+          </p>
+          <p className="text-base leading-7 text-gray-600">
+            Description: {data?.description}
+          </p>
+          <p className="text-base leading-7 text-gray-600">
+            Source: {data?.source}
+          </p>
+          <p className="text-base leading-7 text-gray-600">
+            Step: {data?.step}
+          </p>
+          <p className="text-base leading-7 text-gray-600">
+            Reason: {data?.reason}
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <button
+              onClick={() => navigate("/", { replace: true })}
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">
+              Go back home
+            </button>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
     </>
   );
 }
--- src/pages/OrderSuccessPage.jsx
+++ src/pages/OrderSuccessPage.jsx
@@ -19,10 +19,15 @@ function OrderSuccessPage() {
     {!params.id &&  <Navigate to='/' replace={true}></Navigate>}
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">Order Successfully Placed !!</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Order Number #{params?.id}
+        <p className="text-base sm:text-lg font-semibold text-indigo-600">Order Successfully Placed !!</p>
+        <div className="mt-4 flex flex-col justify-center items-center lg:flex-row">
+        <h1 className=" text-xl sm:3xl font-bold tracking-tight text-gray-900 ">
+          Order Number
         </h1>
+        <h1 className="mt-4 text-xl sm:3xl font-bold tracking-tight text-gray-900">
+            #{params?.id}
+        </h1>
+        </div>
         <p className="mt-6 text-base leading-7 text-gray-600">
           {"You can check your order."}
         </p>
