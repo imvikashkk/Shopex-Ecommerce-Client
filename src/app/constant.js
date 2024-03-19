0d79b177de9e20e4e7629903a2c285a980660320
Vikash Kumar <112840279+imvikashkk@users.noreply.github.com>
2024-03-20 03:12:29 +0530

    change server link with aws server link

--- src/app/constant.js
+++ src/app/constant.js
@@ -1,1 +1,5 @@
-export const BACKEND_URL = "https://api-shopexnow.vercel.app"
+// hosted in vercel
+//export const BACKEND_URL = "https://api-shopexnow.vercel.app"
+
+// Hosted in aws
+export const BACKEND_URL = "http://13.127.160.84:8080"
