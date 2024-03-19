8dc11b885d379aa904741f65ab45a836216b7821
Vikash Kumar <112840279+imvikashkk@users.noreply.github.com>
2024-03-20 04:39:19 +0530

    Update constant.js

--- src/app/constant.js
+++ src/app/constant.js
@@ -2,4 +2,4 @@
 //export const BACKEND_URL = "https://api-shopexnow.vercel.app"
 
 // Hosted in aws
-export const BACKEND_URL = "http://13.127.160.84:8080"
+export const BACKEND_URL = "http://13.200.30.248:8080"
